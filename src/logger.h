077cb93eddb3251731bf13b4be82b0be4b5e28e3
Igor Goremykin <leg3nd1991@gmail.com>
2026-07-13 07:18:47 +0300

    add console logger

---
+++ src/logger.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdio>
+#include <source_location>
+
+#if defined(_MSC_VER)
+    #define __PRETTY_FUNCTION__     __FUNCSIG__
+#endif
+
+#define CONSOLE_COLOR_RESET   "\x1b[0m"
+#define CONSOLE_COLOR_RED     "\x1b[31m"
+#define CONSOLE_COLOR_GREEN   "\x1b[32m"
+#define CONSOLE_COLOR_YELLOW  "\x1b[33m"
+#define CONSOLE_COLOR_BLUE    "\x1b[34m"
+#define CONSOLE_COLOR_MAGENTA "\x1b[35m"
+#define CONSOLE_COLOR_CYAN    "\x1b[36m"
+
+#define log(log_color, log_level, format, ...) \
+    do { \
+        char buffer[256]; \
+        std::snprintf(buffer, sizeof(buffer), format, __VA_ARGS__); \
+        const std::source_location location = std::source_location::current(); \
+        std::string_view file_name{location.file_name()}; \
+        auto pos = file_name.find_last_of("\\/"); \
+        if (pos != std::string_view::npos) { \
+            file_name = file_name.substr(pos + 1); \
+        } \
+        std::printf(" %.*s:%d\t%s%s%s\t%s\n", \
+                    static_cast<int>(file_name.size()), \
+                    file_name.data(), \
+                    __LINE__, \
+                    log_color, \
+                    log_level, \
+                    CONSOLE_COLOR_RESET, \
+                    buffer); \
+    } while (0)
+
+#define log_info(...)  log(CONSOLE_COLOR_CYAN, "INFO", __VA_ARGS__)
+#define log_warn(...)  log(CONSOLE_COLOR_YELLOW, "WARN", __VA_ARGS__)
+#define log_error(...) log(CONSOLE_COLOR_RED, "ERROR", __VA_ARGS__)
