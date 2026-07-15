4320cde3516d850accc28ae42bdb151c49f88a5f
Igor Goremykin <leg3nd1991@gmail.com>
2026-07-16 02:20:07 +0300

    refactor logger

--- src/logger.h
+++ src/logger.h
@@ -10,23 +10,25 @@
 #define CONSOLE_COLOR_MAGENTA "\x1b[35m"
 #define CONSOLE_COLOR_CYAN    "\x1b[36m"
 
-#define log(log_color, log_level, format, ...) \
-    do { \
-        char buffer[256]; \
+#define log(log_color, log_level, format, ...)                      \
+    do                                                              \
+    {                                                               \
+        char buffer[256];                                           \
         std::snprintf(buffer, sizeof(buffer), format, __VA_ARGS__); \
-        std::string_view file_name{__FILE__}; \
-        auto pos = file_name.find_last_of("\\/"); \
-        if (pos != std::string_view::npos) { \
-            file_name = file_name.substr(pos + 1); \
-        } \
-        std::printf(" %.*s:%d\t%s%s%s\t%s\n", \
-                    static_cast<int>(file_name.size()), \
-                    file_name.data(), \
-                    __LINE__, \
-                    log_color, \
-                    log_level, \
-                    CONSOLE_COLOR_RESET, \
-                    buffer); \
+        std::string_view file_name{__FILE__};                       \
+        auto pos = file_name.find_last_of("\\/");                   \
+        if (pos != std::string_view::npos)                          \
+        {                                                           \
+            file_name = file_name.substr(pos + 1);                  \
+        }                                                           \
+        std::printf(" %.*s:%d\t%s%s%s\t%s\n",                       \
+                    static_cast<int>(file_name.size()),             \
+                    file_name.data(),                               \
+                    __LINE__,                                       \
+                    log_color,                                      \
+                    log_level,                                      \
+                    CONSOLE_COLOR_RESET,                            \
+                    buffer);                                        \
     } while (0)
 
 #define log_info(...)  log(CONSOLE_COLOR_CYAN, "INFO", __VA_ARGS__)
